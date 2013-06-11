aeead0e58522cccb4dad87b8e3c0f7899ebc0520
MikeEhman <nkim412@gmail.com>
2013-06-11 11:06:55 -0500

    updated a bunch of features

--- GT.py
+++ GT.py
@@ -19,6 +19,8 @@ from sqlalchemy.orm import *
 from sqlalchemy.ext.declarative import declarative_base
 print "Import Successful: sqlalchemy module"
 
+## --- CONSTANTS --- ##
+DEFAULT_AREA = "라페도 광장"
 
 ## --- SQLAlchemy Setup --- ##
 def sqlalchemy_init():
@@ -27,7 +29,7 @@ def sqlalchemy_init():
     """
     global Base, engine
     Base = declarative_base()
-    engine = create_engine('sqlite:///test.db',echo=True)
+    engine = create_engine('sqlite:///test.db', echo=True)
     
 sqlalchemy_init()
 
@@ -85,17 +87,136 @@ channels_list = xchat.get_list("channels")
 
 class User(Base):
 
+    # Parent: Game
+    # Child: Character
+
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     nick = Column(Unicode)
     host = Column(Unicode)
+    game_id = Column(Integer, ForeignKey('games.id'))
+
+    characters = relationship('Character', backref='users')
     
     def __init__(self, nick, host):
         self.nick = nick
         self.host = host
+        self.game_id = 0
 
     def __repr__(self):
-        return "<User('%s','%s','%s')>" % (self.id, self.name, self.host)
+        return "<User('%s','%s','%s')>" % (self.id, self.nick, self.host)
+
+class Game(Base):
+
+    # Parent: None (Root)
+    # Child: User
+
+    __tablename__ = 'games'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode)
+    hardcore = Column(Unicode)
+    
+    masters = relationship("User")
+    players = relationship("User", backref="games")
+    characters = relationship("Character", backref="game")
+
+    def __init__(self, title, masters, players, hardcore):
+        self.title = title
+        self.masters = masters
+        self.hardcore = hardcore
+        self.players = players
+
+        session.add(self)
+
+        characters_list = []
+        
+        for each_player in players:
+            new_character = Character()
+            new_character.name = unicode(each_player.nick)
+            new_character.games = [self]
+            session.add(new_character)
+            characters_list.append(new_character)
+
+        self.characters = characters_list
+        session.commit()
+
+        players_string = ""
+        for each_player in self.players:
+            if players.index(each_player) < len(players)-1:
+                players_string += each_player.nick + ", "
+            else:
+                players_string += each_player.nick
+
+        context = xchat.get_context()
+        say(context, '[' +self.title + '] ' + '게임이 시작되었습니다. (마스터: ' + self.masters[0].nick + ', ' + '플레이어: ' + players_string + ')') 
+
+class Character(Base):
+
+    # Parent: User, Game
+    # Has to look at user's game_id to see which game it belongs to
+
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    name = Column(Unicode)
+    hp = Column(Integer)
+    cash = Column(Integer)
+    area_id = Column(Integer, ForeignKey('areas.id'))
+    STR = Column(Integer)
+    DEX = Column(Integer)
+    INT = Column(Integer)
+    game_id = Column(Integer, ForeignKey('games.id'))
+
+    def __init__(self):
+        """
+        Initializes the character with default settings.
+        """
+        self.hp = 100
+        self.cash = 0
+
+    def __repr__(self):
+        return "<Character " + self.name + ": HP=%s, Cash=%s>" % (self.hp,self.cash)
+
+    def delta(self,param,num):
+        """
+        Change the given parameter by the given number
+
+        param -- string, changing parameter
+        """
+        exec "self." + param + " += num"
+        session.commit()
+
+    def display_status(self):
+        """
+        displays self's parameters
+        """
+        context = xchat.get_context()
+        say(context, "["+self.name+"] HP: " + str(self.hp) + ", 소지금: " + str(self.cash) + " 해시캐시")
+
+class Area(Base):
+    """
+    Parent: None
+    Children: Characters, Sub-areas...
+    """
+
+    __tablename__ = 'areas'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode)
+
+    east_id = Column(Integer, ForeignKey('areas.id'))
+    north_id = Column(Integer, ForeignKey('areas.id'))
+    west_id = Column(Integer, ForeignKey('areas.id'))
+    south_id = Column(Integer, ForeignKey('areas.id'))
+
+    characters = relationship("Character", backref="area")
+
+    east = relationship("Area", backref=backref("west", remote_side=[id]))
+    north = relationship("Area", backref=backref("south", remote_side=[id]))
+    west = relationship("Area", backref=backref("east", remote_side=[id]))
+    south = relationship("Area", backref=backref("north", remote_side=[id]))
+
+    def __init__(self):
+        pass
 
 sql_create_all_tables()
 
@@ -105,7 +226,7 @@ def say(context, message):
     """
     "Says" the message onto the given context.
     """
-    xchat.command("say " + str(message))
+    context.command("say " + str(message))
 
 def notice(target, message):
     """
@@ -147,6 +268,21 @@ def get_user_object_named(nick):
             return user
     print "No such user..."
 
+def get_user_row_named(nick):
+    """
+    Returns the user row object that has a matching nickname.
+    Uses host name for querying though.
+    """
+    target_host = get_user_object_named(nick).host
+    return session.query(User).filter_by(host=target_host).first()
+
+def get_character_row_named(given_name):
+    """
+    Returns the character row object that has a matching nickname.
+    """
+    print session.query(Character).first()
+    return session.query(Character).filter_by(name=unicode(given_name)).first()
+
 ## === GAME FUNCTIONS === ##
 
 
@@ -160,13 +296,94 @@ def ping(data):
     """
     Pongs back to the target nick
     """
+    context = data["context"]
     nick = data["nick"]
-    xchat.command("say PONG, " + nick)
+    print context
+    print nick
+    say(context, "PONG, " + nick)
+
+# - Session Relative - #
+
+game_on = False
+
+def start_game(data):
+    """
+    Starts a game.
+    First declares a new Game object,
+    Second maps the users to new characters
+    """
+    if game_on:
+        context = data["context"]
+        say(context, "이미 게임이 진행중입니다.")
+    else:
+        # re-parse the parameters
+        params = " ".join(data["params"])
+        print params
+        params = params.split('"')[1::2]
+        for i in xrange(len(params)-1):
+            print params[i]
+        title = params[0]
+        player_nicks = params[1].split(" ")
+        if params[2]:
+            hardcore = params[2]
+
+        # declare a new game
+
+        master_nick = unicode(data["nick"])
+        master_host = unicode(get_user_object_named(master_nick).host)
+        masters = [session.query(User).filter_by(host=master_host).first()]
+
+        players_list = []            
+        for each_player_nick in player_nicks:
+            each_player_host = unicode(get_user_object_named(each_player_nick).host)
+            each_player = session.query(User).filter_by(host=each_player_host).first()
+            players_list.append(each_player)
+
+        game = Game(title, masters, players_list, False)
+
+
+def start_game_session(data):
+    """
+    Starts a session in the given game.
+    """
+    if game_on:
+        context = data["context"]
+        say(context, "이미 게임이 진행중입니다.")
+    else:
+        pass
+    
+# - Initialization and Designation - #
+def init_game():
+    """
+    Initializing the game and mapping the variables.
+    """
+    pass
+
+
+## --- Character Functions --- ##
+
+def damage(data):
+    """
+    damages the given character in the data
+    """
+    char = data["char"]
+    hp_delta = int(data["params"][0])
+    char.delta("hp", -hp_delta)
+
+
 
 ## --- Mappers --- ##
 
 func_map = {unicode("#$테스트"):test,
-            unicode("#$핑"):ping}
+            unicode("#$핑"):ping,
+            unicode("#$게임시작"):start_game}
+
+master_func_map = {}
+
+# only masters can operate character functions
+char_func_map = {unicode("데미지"):damage}
+
+starting_phase = [False, False, False]
 
 def interpret_channel_message(nick, message):
     """
@@ -175,13 +392,42 @@ def interpret_channel_message(nick, message):
     to execute. Other words are passed in as
     parameters.
     """
-    
-    message_list = message.split(" ")
-    func = message_list[0]
-    data = {"nick":nick}
-    if message_list[1:] != []:
-        data[params] = message_list[1:]
-    func_map[func](data)
+
+    if message.find("#$") != -1:        
+        message_list = message.split(" ")
+        func = message_list[0]
+        data = {"nick":nick}
+        data["context"] = xchat.get_context()
+        if message_list[1:] != []:
+            data["params"] = message_list[1:]
+        try:
+            func_map[func](data)
+        except KeyError:
+            # #$<Player> <function>
+            message_list = message.partition("#$")[2].split(" ")
+            char_nick = message_list[0]
+            char = get_character_row_named(char_nick)
+            if message_list[1:] != []:
+                data = {}
+                data["char"] = char
+                char_func = message_list[1]
+                if message_list[2:] != []:
+                    data["params"] = message_list[2:]
+                    try:
+                        char_func_map[char_func](data)
+                    except KeyError:
+                        print "Not a Character Function"   
+            else:
+                char.display_status()
+            
+        # Starting game #
+        if starting_phase[1]:
+            pass
+        elif starting_phase[2]:
+            pass
+
+
+
 
 ## --- Event Handlers --- ##
 
